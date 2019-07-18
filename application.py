c2a2d0d99f7c11f60f2695704f0f6598fe866496
ahmadmustafaanis <ahmadanis5050@gmail.com>
2019-07-18 15:17:41 +0500

    Flask Application Part Done

---
+++ application.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+from flask import Flask , render_template , request, jsonify
+
+from flask_socketio import SocketIO , emit
+
+app=FLask(__name__)
+
+app.config["SECRET_KEY"]=os.getenv("SECRET_KEY")
+soceketio=SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socket.on("submit vote")
+def vote("data"):
+    selection=data["selection"]
+    emit("announce vote" , {"selection":selection},broadcast=True)
