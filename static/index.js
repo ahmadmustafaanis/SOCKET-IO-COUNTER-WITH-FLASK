b41de24a9a3140d06c61be9536d04bfa9ee4f684
ahmadmustafaanis <ahmadanis5050@gmail.com>
2019-07-23 15:31:43 +0500

    final touches

--- static/index.js
+++ static/index.js
@@ -1,19 +1,22 @@
 document.addEventListner('DOMCONTENTLOADED', ()=>{
 
-var socket=io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-socket.on('connect', ()=>{
-  document.querySelectorAll('button').forEach(button =>(){
-    button.onClick =() =>{
-      const selection = button.dataset.vote;
-      socket.emit('submit vote', {'selection':selection});
-    };
-  });
-});
-socket.emit('announce vote' , data=>{
-  const li=document.createElement('li')
-  li.innerHTML=`Vote Recorded ${data.selection}`
-  document.querySelector('#votes').append(li)
+socket.on('connect', () => {
+
+    // Each button should emit a "submit vote" event
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = () => {
+            const selection = button.dataset.vote;
+            socket.emit('submit vote', {'selection': selection});
+        };
+    });
 });
 
+// When a new vote is announced, add to the unordered list
+socket.on('announce vote', data => {
+    const li = document.createElement('li');
+    li.innerHTML = `Vote recorded: ${data.selection}`;
+    document.querySelector('#votes').append(li);
+});
 });
